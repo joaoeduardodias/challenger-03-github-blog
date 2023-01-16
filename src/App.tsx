a0339add13e7b6b1be0b440a6f786b55ae571a7b
João Dias <joaoeduardodias123@gmail.com>
2023-01-16 14:05:48 -0300

    feat: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalTheme } from './styles/global'
 import { defaultTheme } from './styles/themes/defaultTheme'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalTheme />
     </ThemeProvider>
   )
